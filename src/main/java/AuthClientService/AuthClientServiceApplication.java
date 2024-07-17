a7c6f528cd399c56af3c86171ac7c7ec82898c65
mariog44 <mg441236@gmail.com>
2024-07-16 20:19:10 -0500

    PROYECTO BASE. Se creó la estructura del proyecto base para el micro servicio del cliente de autenticaciones de SICAES.

---
+++ src/main/java/AuthClientService/AuthClientServiceApplication.java
@@ -0,0 +1,13 @@
+package AuthClientService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthClientServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/AuthClientService/AuthClientServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package AuthClientService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthClientServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
